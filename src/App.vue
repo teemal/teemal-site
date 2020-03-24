f0198098d18ae323df2162e2d34a4ab2f9bc7f92
teemal <taymal1987@gmail.com>
2020-03-23 19:03:12 -0700

    cleaned up navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    
+    <NavBar />
     <router-view/>
   </div>
 </template>
+<script>
+import NavBar from './components/NavBar'
+export default {
+  components:{
+    NavBar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
