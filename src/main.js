e4edf631efde37b08c843fbf4e2e9aa8c01677e1
teemal <taymal1987@gmail.com>
2020-06-19 10:43:02 -0600

    import firebase

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './../node_modules/bulma/css/bulma.css'
-// require('dotenv').config()
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false
+
+firebase.initializeApp(process.env.VUE_APP_FIREBASE_CONFIG);
 
 new Vue({
   router,
